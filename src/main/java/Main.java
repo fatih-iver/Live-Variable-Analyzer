991864074dfa351ed2344d138ec7f521122647cf
Fatih İver <fatih.iver@boun.edu.tr>
2020-04-07 19:08:15 +0300

    implement lva method visitor template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,44 @@
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodNode;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
-     public static void main(String[] args) {
+     public static void main(String[] args) throws IOException {
+
         System.out.println("Live Variable Analyzer");
+
+         ClassReader classReader = new ClassReader("TestData");
+         ClassNode classNode = new ClassNode();
+         classReader.accept(classNode, ClassReader.EXPAND_FRAMES);
+
+         List<MethodNode> methodNodeList = classNode.methods;
+
+         for (MethodNode methodNode : methodNodeList) {
+             System.out.println();
+             System.out.println(methodNode.name);
+             InsnList insnList = methodNode.instructions;
+             for (AbstractInsnNode abstractInsnNode : insnList) {
+                 System.out.println(abstractInsnNode);
+             }
+         }
+
+
+         for (MethodNode methodNode : methodNodeList) {
+             System.out.println();
+             System.out.println(methodNode.name);
+             methodNode.accept(new LVAMethodVisitor(Opcodes.ASM4));
+         }
+
+
      }
+
 
 }
